27e76946743e456db52e85f99657955d91077357
Connor <connorturlan@gmail.com>
2022-07-06 15:57:17 +0930

    fixed floating point numbers

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -27,7 +27,7 @@ class Calculator {
 
 	calculate() {
 		// convert the buffer to an int.
-		let b = parseFloat(this.buffer);
+		let b = parseFloat(this.buffer || this.result);
 
 		// parse the operator.
 		this.result =
